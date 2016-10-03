8cec5b9b53e77e43300c8ad626b506b37a095d84
exclusiveLogin <alter4444@gmail.com>
2016-10-03 16:09:41 +0400

    fix ip conflict

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function SocToNBRecon(){
         setTimeout(SocToNBRecon,10000);
     }else{
         console.log("SocketCL init");
-        socketCl = require('socket.io-client')('http://10.210.30.148:3001');
+        socketCl = require('socket.io-client')('http://10.210.30.211:3001');
         //***********************CLIENT*************************
 
         socketCl.on('connect', function(){
